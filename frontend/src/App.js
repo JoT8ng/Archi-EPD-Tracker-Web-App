830f223402a67fcc36d744f04d1175943ce00252
Jocelyn Tang <jocelynt8ng@gmail.com>
2023-06-17 21:56:30 +0100

    add home page and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/home';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
@@ -26,32 +28,24 @@ function App() {
 }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <p>To retrieve backend test data:</p>
-        <button onClick={getData}>Click Me</button>
-        {testdata &&
-        <div>
-          <p>Data 1: {testdata.data_1}</p>
-          <p>Data 2: {testdata.data_2}</p>
-        </div>
-        }
-
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
+      <div>
+        <header>
+          <p>To retrieve backend test data:</p>
+          <button onClick={getData}>Click Me</button>
+          {testdata &&
+          <div>
+            <p>Data 1: {testdata.data_1}</p>
+            <p>Data 2: {testdata.data_2}</p>
+          </div>
+          }
+        </header>
+      </div>
+    </Router>
   );
 }
 
