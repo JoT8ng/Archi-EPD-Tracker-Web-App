8899adc152d8d69674956548d4745eb2455e221d
Jocelyn Tang <jocelynt8ng@gmail.com>
2023-07-29 22:04:08 +0100

    add input required

--- frontend/src/pages/tracker.js
+++ frontend/src/pages/tracker.js
@@ -115,13 +115,13 @@ const Tracker = () => {
                 </div>
                 <div class='input2'>
                     <h3>Product Name</h3>
-                    <input id='product_name' name='product_name' type='text' placeholder='Name' defaultValue="None"></input>
+                    <input id='product_name' name='product_name' type='text' placeholder='Name' required></input>
                     <h3>Material Name</h3>
-                    <input id='material_name' name='material_name' type='text' placeholder='Name'></input>
+                    <input id='material_name' name='material_name' type='text' placeholder='Name' required></input>
                 </div>
                 <div class='input3'>
                     <h3>Manufacturer</h3>
-                    <input id='manufacturer' name='manufacturer' type='text' placeholder='Manufacturer'></input>
+                    <input id='manufacturer' name='manufacturer' type='text' placeholder='Manufacturer' required></input>
                 </div>
                 <div class='input4'>
                     <h1>Global Warming Potential &#40;GWP&#41; kg C02 -eq</h1>
@@ -129,16 +129,16 @@ const Tracker = () => {
                 <div class='input5'>
                     <h3>EPD Declared Unit</h3>
                     <h3>Declared Unit</h3>
-                    <input id='declared_unit' name='declared_unit' type='text' placeholder='eg. one ton of cold rolled stainless steel'></input>
+                    <input id='declared_unit' name='declared_unit' type='text' placeholder='eg. one ton of cold rolled stainless steel' required></input>
                     <h3>Value 1</h3>
-                    <input id='value1' name='value1' type='number' placeholder='Number Value' min='1'></input>
+                    <input id='value1' name='value1' type='number' placeholder='Number Value' required></input>
                     <h3>Unit 1</h3>
                     <select id="unit1" name="unit1">
                         <option value="kg">kg</option>
                         <option value="m2">m2</option>
                     </select>
                     <h3>Value 2</h3>
-                    <input id='value2' name='value2' type='number' placeholder='Number Value'></input>
+                    <input id='value2' name='value2' type='number' placeholder='Number Value' required></input>
                     <h3>Unit 2</h3>
                     <select id="unit2" name="unit2">
                         <option value="kg">kg</option>
@@ -147,43 +147,43 @@ const Tracker = () => {
                 </div>
                 <div class='input6'>
                     <h3>Volume of Material in Project &#40;m3&#41;</h3>
-                    <input id='mat_volume' name='mat_volume' type='number' placeholder='Volume' min='1'></input>
+                    <input id='mat_volume' name='mat_volume' type='number' placeholder='Volume' required></input>
                 </div>
                 <div class='input7'>
                     <h3>A1-A3</h3>
-                    <input id='a1to3' name='a1to3' type='number' placeholder='GWP' min='1'></input>
+                    <input id='a1to3' name='a1to3' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input8'>
                     <h3>A4</h3>
-                    <input id='a4' name='a4' type='number' placeholder='GWP' min='1'></input>
+                    <input id='a4' name='a4' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input9'>
                     <h3>A5</h3>
-                    <input id='a5' name='a5' type='number' placeholder='GWP' min='1'></input>
+                    <input id='a5' name='a5' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input10'>
                     <h3>B1</h3>
-                    <input id='b1' name='b1' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b1' name='b1' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input11'>
                     <h3>B2</h3>
-                    <input id='b2' name='b2' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b2' name='b2' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input12'>
                     <h3>B3</h3>
-                    <input id='b3' name='b3' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b3' name='b3' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input13'>
                     <h3>B4</h3>
-                    <input id='b4' name='b4' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b4' name='b4' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input14'>
                     <h3>B5</h3>
-                    <input id='b5' name='b5' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b5' name='b5' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input15'>
                     <h3>B6</h3>
-                    <input id='b6' name='b6' type='number' placeholder='GWP' min='1'></input>
+                    <input id='b6' name='b6' type='number' placeholder='GWP' required></input>
                 </div>
                 <div class='input16'>
                     <button class='button' type='submit'>Add</button>
